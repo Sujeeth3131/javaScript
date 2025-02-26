42407f03085acce86612f3cd797b1171ea3cf0f5
Sujeeth <sujeeth31sujeeth@gmail.com>
2025-02-26 12:48:15 +0530

    first commit

---
+++ test.js
@@ -0,0 +1,23 @@
+//simple if
+console.log("Welcome to college!");
+marks=55
+if(marks>80){
+    console.log("Welcome to tech club!")
+}
+//else if
+age=19
+if(age>=18)
+    console.log("Eligible for voting!");
+else
+    console.log("Not eligible for voting!");
+//nested if
+num=10
+if(num>0){
+    console.log("posiyive");
+    if(num%2==0)
+        console.log("Even");
+    else
+    console.log("odd");
+}
+else
+console.log("Negative");
